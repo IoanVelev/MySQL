69803c02065a558b8ea727f85cd2c994659b26de
Ioan Velev <ioanvelev46@gmail.com>
2024-09-15 17:20:21 +0300

    Update adding_constraints.sql

--- 01. Databases Introduction/adding_constraints.sql
+++ 01. Databases Introduction/adding_constraints.sql
@@ -1,3 +1,11 @@
 ALTER TABLE products
 ADD CONSTRAINT fk_category_id
 FOREIGN KEY (category_id) REFERENCES categories(id);
+
+/*
+ALTER TABLE products
+DROP CONSTRAINT fk_category_id
+
+--> In case you want to remove the constraint
+
+*/
