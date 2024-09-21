6c30d6d92908e24e23c1fd4d5f7886d1be9b1120
Ioan Velev <ioanvelev46@gmail.com>
2024-09-21 19:12:40 +0300

    Insert Records in Both Tables

--- 02. Exercise - Data definition and Datatypes/data_definition_and_datatypes_exercise.sql
+++ 02. Exercise - Data definition and Datatypes/data_definition_and_datatypes_exercise.sql
@@ -19,3 +19,16 @@ ADD COLUMN town_id INT;
 ALTER TABLE minions
 ADD CONSTRAINT fk_town_id
 FOREIGN KEY (town_id) REFERENCES towns(id);
+
+INSERT INTO towns (id, name) VALUES 
+(1, 'Sofia'),
+(2, 'Plovdiv'),
+(3, 'Varna');
+
+INSERT INTO minions VALUES
+(1, 'Kevin', 22, 1),
+(2, 'Bob', 15, 3),
+(3, 'Steward', NULL, 2);
+
+
+SELECT id, name FROM minions;
