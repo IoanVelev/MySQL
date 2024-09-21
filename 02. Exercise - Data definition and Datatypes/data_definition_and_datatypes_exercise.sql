7be627022244e07c92abf532022b506e6544fd0b
Ioan Velev <ioanvelev46@gmail.com>
2024-09-21 18:30:57 +0300

    Create database and tables

---
+++ 02. Exercise - Data definition and Datatypes/data_definition_and_datatypes_exercise.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE minions;
+
+USE minions;
+
+CREATE TABLE minions (
+    id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT
+);
+
+CREATE TABLE towns (
+    town_id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100)
+);
