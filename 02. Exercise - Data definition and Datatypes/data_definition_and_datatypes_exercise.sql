7df5295c338c6ff743a3ba61464944159dd8459a
Ioan Velev <ioanvelev46@gmail.com>
2024-09-27 16:11:10 +0300

    Create new database and new tables with values
    Basic Insert

--- 02. Exercise - Data definition and Datatypes/data_definition_and_datatypes_exercise.sql
+++ 02. Exercise - Data definition and Datatypes/data_definition_and_datatypes_exercise.sql
@@ -251,6 +251,67 @@ INSERT INTO rental_orders(employee_id, customer_id, car_id, car_condition, tank_
 (3, 1, 1, 'Solid', 100, 60000, 70000, 10000, '2024-09-10', '2024-09-13', 1, 5.5, 10, 'Finished', 'Test');
 
 
+CREATE DATABASE soft_uni;
+
+USE soft_uni;
+
+CREATE TABLE towns (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+CREATE TABLE adresses (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    address_text VARCHAR(100),
+    town_id INT,
+    
+    FOREIGN KEY (town_id) REFERENCES towns(id)
+);
+
+CREATE TABLE departments(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+CREATE TABLE employees(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50),
+    middle_name VARCHAR(50),
+    last_name VARCHAR(50),
+    job_title VARCHAR(30),
+    department_id INT,
+    hire_date DATE,
+    salary DOUBLE(10, 2),
+    address_id INT,
+    
+    FOREIGN KEY (department_id) REFERENCES departments(id),
+    FOREIGN KEY (address_id) REFERENCES adresses(id)
+);
+
+
+INSERT INTO towns(name) VALUES
+('Sofia'),
+('Plovdiv'),
+('Varna'),
+('Burgas');
+
+
+INSERT INTO departments(name) VALUES
+('Engineering'),
+('Sales'),
+('Marketing'),
+('Software Development'),
+('Quality Assurance');
+
+INSERT INTO employees(first_name, middle_name, last_name, job_title, department_id, hire_date, salary) VALUES
+('Ivan', 'Ivanov', 'Ivanov', '.NET Developer', 4, '2013-02-01', 3500),
+('Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '2004-03-02', 4000),
+('Maria', 'Petrova', 'Ivanova', 'Intern', 5, '2016-08-28', 525.25),
+('Georgi', 'Terziev', 'Ivanov', 'CEO', 2, '2007-12-09', 3000),
+('Peter', 'Pan', 'Pan', 'Intern', 3, '2016-08-28', 599.88);
+
+SELECT * FROM employees;
+
 
 
 
