928fd84cb3ce8f2c4bb0eb04f8a214dd1736ff20
Ioan Velev <ioanvelev46@gmail.com>
2024-09-27 16:22:44 +0300

    Update data_definition_and_datatypes_exercise.sql

--- 02. Exercise - Data definition and Datatypes/data_definition_and_datatypes_exercise.sql
+++ 02. Exercise - Data definition and Datatypes/data_definition_and_datatypes_exercise.sql
@@ -311,11 +311,32 @@ INSERT INTO employees(first_name, middle_name, last_name, job_title, department_
 ('Peter', 'Pan', 'Pan', 'Intern', 3, '2016-08-28', 599.88);
 
 
+
+SELECT * FROM towns;
+
+SELECT * FROM departments;
+
+SELECT * FROM employees;
+
+
+
+SELECT * FROM towns ORDER BY name;
+
+SELECT * FROM departments ORDER BY name;
+
+SELECT * FROM employees ORDER BY salary DESC;
+
+
+
+
 SELECT name FROM towns ORDER BY name;
 
 SELECT name FROM departments ORDER BY name;
 
 SELECT first_name, last_name, job_title, salary FROM employees ORDER BY salary DESC;
+
+
+
 
 
 
