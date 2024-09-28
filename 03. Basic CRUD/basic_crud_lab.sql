13a155b1910e56421fb498382f0b0d286e033996
Ioan Velev <ioanvelev46@gmail.com>
2024-09-28 15:56:15 +0300

    Aliases rename a table or a column heading

--- 03. Basic CRUD/basic_crud_lab.sql
+++ 03. Basic CRUD/basic_crud_lab.sql
@@ -1,3 +1,29 @@
 SELECT id, first_name, last_name, job_title
 FROM employees
 ORDER BY id;
+
+/*
+Aliases rename a table or a column heading
+*/
+
+SELECT 
+    id AS '#',
+    first_name AS 'First Name',
+    last_name AS 'Last Name',
+    job_title AS 'Job Title'
+FROM employees;
+
+
+SELECT
+    employees.id,
+    employees.first_name,
+    employees.last_name
+FROM hotel.employees;
+
+
+SELECT
+    e.id,
+    e.first_name,
+    e.last_name
+FROM hotel.employees as e;
+
