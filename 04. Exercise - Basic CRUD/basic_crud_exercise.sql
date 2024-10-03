e0f903aef214f8f6b0cc902ca712662e4a802fed
Ioan Velev <ioanvelev46@gmail.com>
2024-10-03 22:48:05 +0300

    Find Full Name of Each Employee

--- 04. Exercise - Basic CRUD/basic_crud_exercise.sql
+++ 04. Exercise - Basic CRUD/basic_crud_exercise.sql
@@ -6,3 +6,6 @@ ORDER BY department_id;
 
 SELECT first_name, last_name, salary FROM employees
 ORDER BY employee_id;
+
+SELECT first_name, middle_name, last_name FROM employees
+ORDER BY employee_id;
