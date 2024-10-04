389c2fbaedd879626364aeab40f97d791d75a629
Ioan Velev <ioanvelev46@gmail.com>
2024-10-04 23:47:56 +0300

    Find Names of All Employees

--- 04. Exercise - Basic CRUD/basic_crud_exercise.sql
+++ 04. Exercise - Basic CRUD/basic_crud_exercise.sql
@@ -22,3 +22,7 @@ ORDER BY employee_id;
 SELECT first_name, last_name, job_title FROM employees
 WHERE salary BETWEEN 20000 AND 30000
 ORDER BY employee_id;
+
+SELECT CONCAT_WS(' ', first_name, middle_name, last_name) AS 'Full Name' FROM employees
+WHERE salary IN (25000, 14000, 12500, 23600);
+
