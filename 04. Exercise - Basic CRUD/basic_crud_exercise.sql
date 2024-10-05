a6f0a9567101f009747440823f28d1c792031e33
Ioan Velev <ioanvelev46@gmail.com>
2024-10-05 18:15:48 +0300

    Display biggest countries by population
    Find the 30 biggest countries by population from Europe.

--- 04. Exercise - Basic CRUD/basic_crud_exercise.sql
+++ 04. Exercise - Basic CRUD/basic_crud_exercise.sql
@@ -89,5 +89,11 @@ USE geography;
 SELECT peak_name FROM peaks
 ORDER BY peak_name;
 
+SELECT country_name, population FROM countries
+WHERE continent_code = 'EU'
+ORDER BY population DESC, country_name
+LIMIT 30;
+
+
 
 
