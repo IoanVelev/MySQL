18be8e11e932f23d5e1a5f687b397266e9c1a78f
Ioan Velev <ioanvelev46@gmail.com>
2024-10-05 16:31:20 +0300

    Create View Employees with Job Titles

--- 04. Exercise - Basic CRUD/basic_crud_exercise.sql
+++ 04. Exercise - Basic CRUD/basic_crud_exercise.sql
@@ -53,3 +53,7 @@ AS SELECT first_name, last_name, salary FROM employees;
 SELECT * FROM v_employees_salaries;
 
 
+CREATE VIEW v_employees_job_titles
+AS SELECT CONCAT_WS(" ", first_name, middle_name, last_name) AS `full_name`, job_title FROM employees;
+
+SELECT * FROM v_employees_job_titles;
