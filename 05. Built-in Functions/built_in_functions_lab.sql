ed4442190804bc7e339044d51820cd3a82b61c2f
Ioan Velev <ioanvelev46@gmail.com>
2024-10-06 15:47:49 +0300

    Update built_in_functions_lab.sql

--- 05. Built-in Functions/built_in_functions_lab.sql
+++ 05. Built-in Functions/built_in_functions_lab.sql
@@ -7,7 +7,7 @@ ORDER by id;
 Use substring function to extract part of the string and check the first 3 letters;
 */
 
-SELECT REPLACE(title, 'The', '***') AS title
+SELECT REPLACE(title, 'The', '***') AS 'title'
 FROM books
 WHERE SUBSTRING(title, 1, 3) = 'The'
 ORDER BY id;
