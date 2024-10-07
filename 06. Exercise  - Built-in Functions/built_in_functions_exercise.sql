dae495aa5d17faa058b7c0668af6327bd32effa3
Ioan Velev <ioanvelev46@gmail.com>
2024-10-07 17:02:58 +0300

    Find First Names of All Employess

--- 06. Exercise  - Built-in Functions/built_in_functions_exercise.sql
+++ 06. Exercise  - Built-in Functions/built_in_functions_exercise.sql
@@ -1,3 +1,18 @@
+-- Problem 1
 SELECT first_name, last_name FROM employees
 WHERE first_name LIKE 'Sa%'
+ORDER BY employee_id;
+
+-- Problem 2
+SELECT first_name, last_name FROM employees
+WHERE last_name LIKE '%ei%'
+ORDER BY employee_id; -- 1st approach
+
+SELECT first_name, last_name FROM employees
+WHERE last_name REGEXP 'ei'
+ORDER BY employee_id; -- 2nd approach
+
+-- Problem 3
+SELECT first_name FROM employees
+WHERE department_id IN(3, 10) AND YEAR(hire_date) BETWEEN 1995 AND 2005
 ORDER BY employee_id;
