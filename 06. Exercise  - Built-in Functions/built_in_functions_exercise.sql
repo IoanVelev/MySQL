76484e9f4a10edb3cef1f3a82d727f5b07fe9e89
Ioan Velev <ioanvelev46@gmail.com>
2024-10-07 18:49:51 +0300

    Solve problem 11
    Mix of Peak and River Names

--- 06. Exercise  - Built-in Functions/built_in_functions_exercise.sql
+++ 06. Exercise  - Built-in Functions/built_in_functions_exercise.sql
@@ -66,5 +66,13 @@ SELECT country_name, iso_code FROM countries
 WHERE country_name LIKE '%A%A%A%'
 ORDER BY iso_code;
 
+-- Problem 11
+SELECT peak_name, river_name,
+LOWER(CONCAT(peak_name, SUBSTRING(river_name, 2))) AS 'mix'
+FROM peaks, rivers
+WHERE RIGHT(peak_name, 1) LIKE LEFT(river_name, 1)
+ORDER BY mix;
+
+
 
 
