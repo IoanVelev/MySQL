5ddb9e0ae22b4c54c57d25613e594351e4ad0205
Ioan Velev <ioanvelev46@gmail.com>
2024-10-08 14:16:57 +0300

    Solve problem 15

--- 06. Exercise  - Built-in Functions/built_in_functions_exercise.sql
+++ 06. Exercise  - Built-in Functions/built_in_functions_exercise.sql
@@ -88,3 +88,18 @@ ORDER BY email_provider, user_name;
 SELECT user_name, ip_address FROM users
 WHERE ip_address LIKE '___.1%.%.___'
 ORDER BY user_name;
+
+-- Problem 15
+SELECT name,
+CASE 
+    WHEN HOUR(start) >= 0 AND HOUR(start) < 12 THEN 'Morning'
+    WHEN HOUR(start) >= 12 AND HOUR(start) < 18 THEN 'Afternoon'
+    WHEN HOUR(start) >= 18 AND HOUR(start) < 24 THEN 'Evening'
+END AS `Part of the Day`,
+CASE
+    WHEN duration <= 3 THEN 'Extra Short'
+    WHEN duration <= 6 THEN 'Short'
+    WHEN duration <= 10 THEN 'Long'
+    ELSE 'Extra Long'
+END AS `duration`
+FROM games;
