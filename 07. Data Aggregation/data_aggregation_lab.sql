453344f107ead402eb83d072e70870b265a319de
Ioan Velev <ioanvelev46@gmail.com>
2024-10-08 19:01:27 +0300

    Solve problem 5
    Retrieve information about the prices of each category.

--- 07. Data Aggregation/data_aggregation_lab.sql
+++ 07. Data Aggregation/data_aggregation_lab.sql
@@ -35,3 +35,10 @@ SELECT COUNT(category_id) AS appetizers_count FROM products
 WHERE category_id = 2 AND price > 8;
 
 
+-- Problem 5
+SELECT category_id,
+ROUND(AVG(price), 2) AS `Average Price`,
+ROUND(MIN(price), 2) AS `Cheapest Product`,
+ROUND(MAX(price), 2) AS `Most Expensive Product`
+FROM products
+GROUP BY category_id;
