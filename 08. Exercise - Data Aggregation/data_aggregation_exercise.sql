2730780a0406f94ba79afaa03580e58aea00addf
Ioan Velev <ioanvelev46@gmail.com>
2024-10-08 22:27:06 +0300

    Solve problem 3 and 4

--- 08. Exercise - Data Aggregation/data_aggregation_exercise.sql
+++ 08. Exercise - Data Aggregation/data_aggregation_exercise.sql
@@ -3,3 +3,17 @@ SELECT COUNT(id) AS count FROM wizzard_deposits;
 
 -- Problem 2
 SELECT MAX(magic_wand_size) AS `longest_magic_wand` FROM wizzard_deposits;
+
+-- Problem 3
+SELECT deposit_group, MAX(magic_wand_size) AS `longest_magic_wand`
+FROM wizzard_deposits
+GROUP BY deposit_group
+ORDER BY longest_magic_wand, deposit_group;
+
+-- Problem 4
+SELECT * FROM wizzard_deposits;
+SELECT deposit_group
+FROM wizzard_deposits
+GROUP BY deposit_group
+HAVING MIN(magic_wand_size)
+LIMIT 1;
