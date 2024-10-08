465f0e0233811e14e2ca951e82d331a5ea5018fd
Ioan Velev <ioanvelev46@gmail.com>
2024-10-08 17:19:20 +0300

    Update data_aggregation_lab.sql

--- 07. Data Aggregation/data_aggregation_lab.sql
+++ 07. Data Aggregation/data_aggregation_lab.sql
@@ -10,4 +10,8 @@ ORDER BY department_id;
 
 SELECT department_id, MAX(salary) AS `max_salary`
 FROM employees
-GROUP BY department_id;
+GROUP BY department_id; -- Display departments with their highest employee salary
+
+SELECT department_id, MIN(salary) AS `min_salary`
+FROM employees
+GROUP BY department_id; -- Display departments with their lowest employee salary
