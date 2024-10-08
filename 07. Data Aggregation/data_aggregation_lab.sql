38e20d08446beaaf58afc768fdefc1d64b95170e
Ioan Velev <ioanvelev46@gmail.com>
2024-10-08 17:16:01 +0300

    Update data_aggregation_lab.sql

--- 07. Data Aggregation/data_aggregation_lab.sql
+++ 07. Data Aggregation/data_aggregation_lab.sql
@@ -7,3 +7,7 @@ SELECT department_id, COUNT(id) AS `Number of employees`
 FROM employees
 GROUP BY department_id
 ORDER BY department_id;
+
+SELECT department_id, MAX(salary) AS `max_salary`
+FROM employees
+GROUP BY department_id;
