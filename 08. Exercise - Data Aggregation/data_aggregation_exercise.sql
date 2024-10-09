e845b306dad4ccd35d726712e1fa479df88f7c25
Ioan Velev <ioanvelev46@gmail.com>
2024-10-09 16:24:55 +0300

    Solve problem 14

--- 08. Exercise - Data Aggregation/data_aggregation_exercise.sql
+++ 08. Exercise - Data Aggregation/data_aggregation_exercise.sql
@@ -103,3 +103,10 @@ SELECT department_id, AVG(salary) AS `avg_salary`
 FROM high_paid_employees
 GROUP BY department_id
 ORDER BY department_id;
+
+-- Problem 14
+SELECT department_id, MAX(salary) AS `max_salary`
+FROM employees
+GROUP BY department_id
+HAVING max_salary NOT BETWEEN 30000 AND 70000
+ORDER BY department_id;
