4db787da4a5d5b928bc5ab7fe10e23c0bd358445
Ioan Velev <ioanvelev46@gmail.com>
2024-10-09 17:38:20 +0300

    Solve problem 17

--- 08. Exercise - Data Aggregation/data_aggregation_exercise.sql
+++ 08. Exercise - Data Aggregation/data_aggregation_exercise.sql
@@ -126,3 +126,12 @@ FROM employees AS e
 GROUP BY e.department_id
 HAVING third_highest_salary
 ORDER BY e.department_id;
+
+-- Problem 17
+SELECT first_name, last_name, department_id FROM employees AS e
+WHERE salary > (
+			    SELECT AVG(salary) FROM employees
+                WHERE department_id = e.department_id
+)
+ORDER BY department_id, employee_id
+LIMIT 10;
