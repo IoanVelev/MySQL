62ace5efbe1cafbf70f1af2933c0ee4c3957b886
Ioan Velev <ioanvelev46@gmail.com>
2024-10-11 00:24:13 +0300

    Solve problem 4

--- 09. Table Relations/table_relations_lab.sql
+++ 09. Table Relations/table_relations_lab.sql
@@ -20,3 +20,34 @@ v.vehicle_type,
 CONCAT(c.first_name, ' ', c.last_name) AS `driver_name`
 FROM vehicles AS v
 JOIN campers AS c ON v.driver_id = c.id;
+
+-- Problem 3
+
+
+-- Problem 4
+DROP TABLE peaks, mountains;
+
+CREATE TABLE mountains(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE peaks(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    mountain_id INT,
+    
+    FOREIGN KEY (mountain_id) REFERENCES mountains(id)
+    ON DELETE CASCADE
+);
+
+INSERT INTO mountains(name) VALUES
+('Vitosha'),
+('Rila');
+
+INSERT INTO peaks(name, mountain_id) VALUES
+('Cherni vrah', 1),
+('Musala', 2),
+('Malyovica', 2);
+
+DELETE FROM mountains WHERE id = 2;
