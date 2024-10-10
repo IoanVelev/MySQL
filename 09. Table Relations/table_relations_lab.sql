58c1e981a468ce3791030c2b079c364c76ec0f20
Ioan Velev <ioanvelev46@gmail.com>
2024-10-10 16:48:07 +0300

    Solve problem 1

--- 09. Table Relations/table_relations_lab.sql
+++ 09. Table Relations/table_relations_lab.sql
@@ -1,11 +1,12 @@
+-- Problem 1
 CREATE TABLE mountains(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL UNIQUE
+    name VARCHAR(50)
 );
 
 CREATE TABLE peaks(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL UNIQUE,
+    name VARCHAR(50),
     mountain_id INT,
     
     CONSTRAINT fk_peaks_mountains
