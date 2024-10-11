f673cfeb43919a4a8c393688f6ddb3ca01da5e8c
Ioan Velev <ioanvelev46@gmail.com>
2024-10-11 17:23:44 +0300

    Solve problem 8

--- 12. Exercise - Subqueries and JOINs/subqueries_and_joins_ex.sql
+++ 12. Exercise - Subqueries and JOINs/subqueries_and_joins_ex.sql
@@ -65,3 +65,14 @@ ON p.project_id = ep.project_id
 WHERE DATE(p.start_date) > '2002/08/13' AND p.end_date IS NULL
 ORDER BY e.first_name, p.name
 LIMIT 5;
+
+-- Problem 8
+SELECT e.employee_id, e.first_name,
+    IF(YEAR(p.start_date) >= 2005, NULL, p.name) AS `project_name`
+FROM employees e
+JOIN employees_projects ep
+ON ep.employee_id = e.employee_id
+JOIN projects p
+ON p.project_id = ep.project_id
+WHERE ep.employee_id = 24
+ORDER BY p.name;
