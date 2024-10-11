6e93fcb03656c137742001b733aff51c4a252e82
Ioan Velev <ioanvelev46@gmail.com>
2024-10-11 17:39:39 +0300

    Solve problem 9

--- 12. Exercise - Subqueries and JOINs/subqueries_and_joins_ex.sql
+++ 12. Exercise - Subqueries and JOINs/subqueries_and_joins_ex.sql
@@ -76,3 +76,11 @@ JOIN projects p
 ON p.project_id = ep.project_id
 WHERE ep.employee_id = 24
 ORDER BY p.name;
+
+-- Problem 9
+SELECT e.employee_id, e.first_name, e.manager_id, m.first_name AS `manager_name`
+FROM employees e
+JOIN employees m
+ON m.employee_id = e.manager_id
+WHERE e.manager_id IN (3, 7)
+ORDER BY e.first_name;
