e543aac6e955d226674e5e286d63d7e85378dad4
Ioan Velev <ioanvelev46@gmail.com>
2024-10-11 18:12:27 +0300

    Solve problem 10

--- 12. Exercise - Subqueries and JOINs/subqueries_and_joins_ex.sql
+++ 12. Exercise - Subqueries and JOINs/subqueries_and_joins_ex.sql
@@ -84,3 +84,16 @@ JOIN employees m
 ON m.employee_id = e.manager_id
 WHERE e.manager_id IN (3, 7)
 ORDER BY e.first_name;
+
+-- Problem 10
+SELECT e.employee_id,
+CONCAT_WS(' ', e.first_name, e.last_name) AS employee_name,
+CONCAT_WS(' ', m.first_name, m.last_name) AS `manager_name`,
+d.name
+FROM employees e
+JOIN employees m
+ON m.employee_id = e.manager_id
+JOIN departments d
+ON d.department_id = e.department_id
+ORDER BY e.employee_id
+LIMIT 5;
