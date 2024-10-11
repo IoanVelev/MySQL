575935f641107f822779d8b531493ff1adba230f
Ioan Velev <ioanvelev46@gmail.com>
2024-10-11 13:09:19 +0300

    Solve problem 3

--- 09. Table Relations/table_relations_lab.sql
+++ 09. Table Relations/table_relations_lab.sql
@@ -22,7 +22,12 @@ FROM vehicles AS v
 JOIN campers AS c ON v.driver_id = c.id;
 
 -- Problem 3
-
+SELECT r.starting_point AS `route_starting_point`,
+r.end_point AS `route_end_point`,
+r.leader_id,
+CONCAT(c.first_name, ' ', c.last_name) AS `leader_name`
+FROM routes AS r
+JOIN campers AS c ON c.id = r.leader_id;
 
 -- Problem 4
 DROP TABLE peaks, mountains;
