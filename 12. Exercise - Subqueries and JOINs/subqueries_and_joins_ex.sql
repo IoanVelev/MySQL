ff0733a24c8075e489f8153a253888da772807a5
Ioan Velev <ioanvelev46@gmail.com>
2024-10-11 14:03:33 +0300

    Solve problem 2

--- 12. Exercise - Subqueries and JOINs/subqueries_and_joins_ex.sql
+++ 12. Exercise - Subqueries and JOINs/subqueries_and_joins_ex.sql
@@ -8,3 +8,12 @@ JOIN addresses AS a ON a.address_id = e.address_id
 ORDER BY e.address_id
 LIMIT 5;
 
+-- Problem 2
+SELECT e.first_name, e.last_name, t.name, a.address_text
+FROM employees e
+JOIN addresses a 
+ON a.address_id = e.address_id
+JOIN towns t 
+ON t.town_id = a.town_id 
+ORDER BY first_name, last_name
+LIMIT 5;
