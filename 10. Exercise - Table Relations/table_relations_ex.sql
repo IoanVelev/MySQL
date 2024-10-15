4b4b1c4b9b4f2f398416efad938e2b83de577d69
Ioan Velev <ioanvelev46@gmail.com>
2024-10-15 23:16:15 +0300

    Solve problem 5

--- 10. Exercise - Table Relations/table_relations_ex.sql
+++ 10. Exercise - Table Relations/table_relations_ex.sql
@@ -110,3 +110,48 @@ INSERT INTO teachers VALUES
 ALTER TABLE teachers
 ADD FOREIGN KEY (manager_id)
 REFERENCES teachers(teacher_id);
+
+
+-- Problem 5
+CREATE TABLE items_types(
+    item_type_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+CREATE TABLE items(
+    item_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    item_type_id INT,
+    
+    FOREIGN KEY (item_type_id) REFERENCES items_types(item_type_id)
+);
+
+CREATE TABLE cities(
+    city_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+CREATE TABLE customers(
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    birthday DATE,
+    city_id INT,
+    
+    FOREIGN KEY (city_id) REFERENCES cities(city_id)
+);
+
+CREATE TABLE orders(
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+CREATE TABLE order_items(
+    order_id INT,
+    item_id INT,
+    
+    PRIMARY KEY (order_id, item_id),
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (item_id) REFERENCES items(item_id)
+);
