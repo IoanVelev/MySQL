9fa2aeafdd7c041592f352fd2275eeeecd183faa
Ioan Velev <ioanvelev46@gmail.com>
2024-10-15 22:34:25 +0300

    Solve problem 3

--- 10. Exercise - Table Relations/table_relations_ex.sql
+++ 10. Exercise - Table Relations/table_relations_ex.sql
@@ -51,3 +51,41 @@ INSERT INTO models VALUES
 (104, 'Model X', 2),
 (105, 'Model 3', 2),
 (106, 'Nova', 3);
+
+-- Problem 3
+CREATE TABLE students(
+    student_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+CREATE TABLE exams(
+    exam_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+CREATE TABLE students_exams(
+    student_id INT,
+    exam_id INT,
+    
+    PRIMARY KEY (student_id, exam_id),
+    FOREIGN KEY (student_id) REFERENCES students(student_id),
+    FOREIGN KEY (exam_id) REFERENCES exams(exam_id)
+);
+
+INSERT INTO students VALUES
+(1, 'Mila'),
+(2, 'Toni'),
+(3, 'Ron');
+
+INSERT INTO exams VALUES
+(101, 'Spring MVC'),
+(102, 'Neo4j'),
+(103, 'Oracle 11g');
+
+INSERT INTO students_exams VALUES
+(1, 101),
+(1, 102),
+(2, 101),
+(3, 103),
+(2, 102),
+(2, 103);
