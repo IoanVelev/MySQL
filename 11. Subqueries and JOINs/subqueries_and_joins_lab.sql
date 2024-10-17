9f63aa4ca14c57e25f6930f492d4514cae7484dd
Ioan Velev <ioanvelev46@gmail.com>
2024-10-17 15:05:54 +0300

    Solve problem 2

--- 11. Subqueries and JOINs/subqueries_and_joins_lab.sql
+++ 11. Subqueries and JOINs/subqueries_and_joins_lab.sql
@@ -1,3 +1,4 @@
+-- Problem 1
 SELECT e.employee_id,
 CONCAT(e.first_name, ' ', e.last_name) AS `full_name`,
 d.department_id,
@@ -7,3 +8,13 @@ JOIN departments d
 ON d.manager_id = e.employee_id
 ORDER BY e.employee_id
 LIMIT 5;
+
+-- Problem 2
+SELECT t.town_id,
+t.name,
+a.address_text
+FROM towns t
+JOIN addresses a
+ON a.town_id = t.town_id
+WHERE t.name IN ('San Francisco', 'Sofia', 'Carnation')
+ORDER BY t.town_id, a.address_id;
