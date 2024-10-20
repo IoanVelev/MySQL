fea2685938c51ad7fe891a2f6410bb483e0e3737
Ioan Velev <ioanvelev46@gmail.com>
2024-10-20 17:14:21 +0300

    Solve problem 15

--- 12. Exercise - Subqueries and JOINs/subqueries_and_joins_ex.sql
+++ 12. Exercise - Subqueries and JOINs/subqueries_and_joins_ex.sql
@@ -135,6 +135,15 @@ ORDER BY c.country_name
 LIMIT 5;
 
 -- Problem 15
+SELECT c.continent_code, c.currency_code, COUNT(*) AS `currency_usage` FROM countries c
+GROUP BY c.continent_code, c.currency_code
+HAVING currency_usage > 1 AND currency_usage = (SELECT COUNT(*) AS `currency_usage` FROM countries 
+												WHERE continent_code = c.continent_code
+                                                GROUP BY currency_code
+                                                ORDER BY currency_usage DESC
+                                                LIMIT 1)
+ORDER BY continent_code, currency_code;
+
 
 -- Problem 16
 SELECT COUNT(*) AS `country_count` FROM countries c
