7cb77b55172b60c658c8a7c705663fe93d7c793b
Ioan Velev <ioanvelev46@gmail.com>
2024-10-20 15:27:16 +0300

    Solve problem 17

--- 12. Exercise - Subqueries and JOINs/subqueries_and_joins_ex.sql
+++ 12. Exercise - Subqueries and JOINs/subqueries_and_joins_ex.sql
@@ -134,8 +134,30 @@ WHERE continent_code LIKE 'AF'
 ORDER BY c.country_name
 LIMIT 5;
 
+-- Problem 15
+
 -- Problem 16
 SELECT COUNT(*) AS `country_count` FROM countries c
 LEFT JOIN mountains_countries mc
 ON c.country_code = mc.country_code
 WHERE mountain_id IS NULL;
+
+-- Problem 17
+
+SELECT c.country_name,
+MAX(p.elevation) AS `highest_peak_elevation`,
+MAX(r.length) AS `longest_river_length`
+FROM countries c
+LEFT JOIN mountains_countries mc
+ON mc.country_code = c.country_code
+LEFT JOIN peaks p
+ON p.mountain_id = mc.mountain_id
+LEFT JOIN countries_rivers cr
+ON cr.country_code = c.country_code
+LEFT JOIN rivers r
+ON r.id = cr.river_id
+GROUP BY c.country_name
+ORDER BY highest_peak_elevation DESC,
+longest_river_length DESC,
+c.country_name
+LIMIT 5;
