d4531f8c165dba2d95bdf5215030c3ed389d0fdf
Ioan Velev <ioanvelev46@gmail.com>
2024-10-21 17:56:18 +0300

    Solve problem 3

--- 13. Database Programmability and Transactions/database_programmability_and_transactions.sql
+++ 13. Database Programmability and Transactions/database_programmability_and_transactions.sql
@@ -32,3 +32,15 @@ END$$
 DELIMITER ;
 
 CALL usp_raise_salaries('Finance');
+
+-- Problem 3
+DELIMITER $$
+CREATE PROCEDURE usp_raise_salary_by_id(id INT)
+BEGIN
+    UPDATE employees
+    SET salary = salary * 1.05
+    WHERE employee_id = id;
+END$$
+DELIMITER ;
+
+CALL usp_raise_salary_by_id(17);
