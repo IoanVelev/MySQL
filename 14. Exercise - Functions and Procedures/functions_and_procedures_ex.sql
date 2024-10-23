efedb0b868ec8a4c222a3e84e085b266160708d1
Ioan Velev <ioanvelev46@gmail.com>
2024-10-23 21:16:00 +0300

    Solve problem 2

--- 14. Exercise - Functions and Procedures/functions_and_procedures_ex.sql
+++ 14. Exercise - Functions and Procedures/functions_and_procedures_ex.sql
@@ -1,4 +1,4 @@
-
+-- Problem 1
 DELIMITER $$
 CREATE PROCEDURE usp_get_employees_salary_above_35000()
 BEGIN
@@ -9,3 +9,15 @@ END$$
 DELIMITER ;
 
 CALL usp_get_employees_salary_above_35000();
+
+-- Problem 2
+DELIMITER $$
+CREATE PROCEDURE usp_get_employees_salary_above(given_salary DECIMAL(19, 4))
+BEGIN
+    SELECT first_name, last_name FROM employees
+    WHERE salary >= given_salary
+    ORDER BY first_name, last_name, salary;
+END$$
+DELIMITER ;
+
+CALL usp_get_employees_salary_above(45000);
