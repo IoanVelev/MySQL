adbba23abd29f09fa25f07018133df8ace168825
Ioan Velev <ioanvelev46@gmail.com>
2024-10-29 22:01:28 +0200

    Solve problem 11

--- 15. Exam Preparation/exam_prep.sql
+++ 15. Exam Preparation/exam_prep.sql
@@ -155,3 +155,22 @@ END$$
 DELIMITER ;
 
 SELECT udf_average_salary_by_position_name('Forester') AS `position_average_salary`;
+
+-- Problem 11
+DELIMITER $$
+CREATE PROCEDURE udp_increase_salaries_by_country(country_name VARCHAR(40))
+BEGIN
+    UPDATE workers w
+    JOIN preserves p
+    ON w.preserve_id = p.id
+    JOIN countries_preserves cp
+    ON p.id = cp.preserve_id
+    JOIN countries c
+    ON cp.country_id = c.id
+    SET w.salary = w.salary * 1.05
+    WHERE c.name = country_name;
+END$$
+
+DELIMITER ;
+
+CALL udp_increase_salaries_by_country('Germany');
