0f701c864ccdd445fb73cc08c29a7abf6564b73a
Ioan Velev <ioanvelev46@gmail.com>
2024-11-01 16:26:06 +0200

    Solve problem 2

--- 16. Exam Preparation 2/exam_prep_2.sql
+++ 16. Exam Preparation 2/exam_prep_2.sql
@@ -52,3 +52,13 @@ CREATE TABLE instructors_students(
     FOREIGN KEY (instructor_id) REFERENCES instructors(id),
     FOREIGN KEY (student_id) REFERENCES students(id)
 );
+
+-- SECTION 2 - DML
+-- .02 Insert
+INSERT INTO students(first_name, last_name, age, phone_number)
+SELECT LOWER(REVERSE(first_name)), 
+LOWER(REVERSE(last_name)),
+age + LEFT(phone_number, 1),
+CONCAT("1+", '', phone_number)
+FROM students
+WHERE age < 20;
